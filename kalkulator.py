52740dec14ef11861f16602acadc990fed0df6f3
debskiszymon <debski.szymon@gmail.com>
2020-10-01 18:58:28 +0200

    kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,2 +1,14 @@
-def kalkulator():
-    pass
+def kalkulator(dzialanie, liczba_01, liczba_02):
+    wynik = 0
+    if dzialanie == 1:
+        wynik = liczba_01 + liczba_02
+    elif dzialanie == 2:
+        wynik = liczba_01 - liczba_02
+    elif dzialanie == 3:
+        wynik = liczba_01 * liczba_02
+    elif dzialanie == 4:
+        wynik = liczba_01 / liczba_02
+    return wynik    
+
+print(kalkulator(4, 2, 2))
+        
