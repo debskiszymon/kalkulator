b743c328fc62519d7971ad7d85d4579cac71fab4
debskiszymon <debski.szymon@gmail.com>
2020-10-01 19:21:18 +0200

    kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,3 +1,6 @@
+import logging
+logging.basicConfig(level=logging.DEBUG, format="")
+
 def kalkulator(dzialanie, liczba_01, liczba_02):
     wynik = 0
     if dzialanie == 1:
@@ -10,5 +13,18 @@ def kalkulator(dzialanie, liczba_01, liczba_02):
         wynik = liczba_01 / liczba_02
     return wynik    
 
-print(kalkulator(4, 2, 2))
-        
+#print(kalkulator(4, 2, 2))
+
+if __name__ == "__main__": 
+    dzialanie = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:"))
+    liczba_01 = int(input("Podaj składnik 1:"))
+    liczba_02 = int(input("Podaj składnik 2:"))
+    if dzialanie == 1:
+        logging.debug(f"Dodaję {liczba_01} do {liczba_02}")
+    elif dzialanie == 2:
+        logging.debug(f"Odejmuję {liczba_01} od {liczba_02}")
+    elif dzialanie == 3:
+        logging.debug(f"Mnożę {liczba_01} razy {liczba_02}")
+    elif dzialanie == 4:
+        logging.debug(f"Dzielę {liczba_01} przez {liczba_02}")
+    print(kalkulator(dzialanie, liczba_01, liczba_02))
