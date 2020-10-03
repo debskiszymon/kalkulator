d73346014c32b2258e3cdf46a6d5cf556acd3b39
debskiszymon <debski.szymon@gmail.com>
2020-10-03 22:31:56 +0200

    kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,63 +1,75 @@
 import logging
 logging.basicConfig(level=logging.DEBUG, format="")
 
-def dodwanie(*argv):
-    wynik = 0
-    for arg in argv:
-        wynik += arg
-    return wynik
-def odejmowanie(*argv):
-    wynik = 0
-    for arg in argv:
-        wynik -= arg
-    return wynik
-def mnozenie(*argv):
-    wynik = argv[0]
-    for x in range(1, len(argv)):
-        wynik *= argv[x]
-    return wynik
-def dzielenie(*argv):
-    wynik = argv[0]
-    for x in range(1, len(argv)):
-        wynik /= argv[x]
-    return wynik
+#calculator function 
 
-#calculator function
-
-def kalkulator(dzialanie, *argv):
-    wynik = 0
+def kalkulator(dzialanie, liczby):
     if dzialanie == 1:
-        wynik = dodwanie(argv)
+        wynik = liczby[0]
+        for x in range(1, len(liczby)):
+            wynik += liczby[x]
+        return wynik
     elif dzialanie == 2:
-        wynik = liczba_01 - liczba_02
+        wynik = liczby[0]
+        for x in range(1, len(liczby)):
+            wynik -= liczby[x]
+        return wynik
     elif dzialanie == 3:
-        wynik = liczba_01 * liczba_02
+        wynik = liczby[0]
+        for x in range(1, len(liczby)):
+            wynik *= liczby[x]
+        return wynik
     elif dzialanie == 4:
-        wynik = liczba_01 / liczba_02
-    return wynik    
+        wynik = liczby[0]
+        for x in range(1, len(liczby)):
+            wynik /= liczby[x]
+        return wynik
 
-#is the input a integer check function
-
-def is_int():
-    while True:
-            try:
-                liczba = int(input("Podaj składnik 1:"))
-                return liczba             
-            except ValueError:
-                print("Proszę podać liczbę!!")
-
-#print(kalkulator(4, 2, 2))
+def info(dzialanie, liczby):
+    if dzialanie == 1:
+        text = "Dodaję "
+        for i in range(len(liczby)-1):
+            text = text + f"{liczby[i]} do "
+        text = text + f"{liczby[len(liczby)-1]}"
+        logging.debug(text)
+    elif dzialanie == 2:
+        text = "Odejmuję "
+        for i in range(len(liczby)-1):
+            text = text + f"{liczby[i]} od "
+        text = text + f"{liczby[len(liczby)-1]}"
+        logging.debug(text)
+    elif dzialanie == 3:
+        text = "Mnożę "
+        for i in range(len(liczby)-1):
+            text = text + f"{liczby[i]} razy "
+        text = text + f"{liczby[len(liczby)-1]}"
+        logging.debug(text)
+    elif dzialanie == 4:
+        text = "Dzielę "
+        for i in range(len(liczby)-1):
+            text = text + f"{liczby[i]} przez "
+        text = text + f"{liczby[len(liczby)-1]}"
+        logging.debug(text)
 
 if __name__ == "__main__": 
     dzialanie = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:"))
-    liczba_01 = is_int()
-    liczba_02 = is_int()
-    if dzialanie == 1:
-        logging.debug(f"Dodaję {liczba_01} do {liczba_02}")
-    elif dzialanie == 2:
-        logging.debug(f"Odejmuję {liczba_01} od {liczba_02}")
-    elif dzialanie == 3:
-        logging.debug(f"Mnożę {liczba_01} razy {liczba_02}")
-    elif dzialanie == 4:
-        logging.debug(f"Dzielę {liczba_01} przez {liczba_02}")
-    print(kalkulator(dzialanie, liczba_01, liczba_02))
+    tries = 5
+    for i in range(6):
+        tries -= 1
+        if tries < 0:
+            print("Przekroczono maxymalną liczbę prób: 5")
+            exit(1)
+        try:
+            liczby = [int(x) for x in input("Proszę podać liczby po przecinkach: ").split(",")]
+            break
+        except ValueError:
+            print("Proszę podać liczbę oraz używać tylko przecinków")
+        
+        
+
+    
+
+wynik = kalkulator(dzialanie, liczby)
+info(dzialanie, liczby)
+print(wynik)
+
